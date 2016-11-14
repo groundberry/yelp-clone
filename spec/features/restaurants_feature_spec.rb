37bbd3cbb36ab7420c96739d2e4db5e844e35cdf
elizabethvenner <elizabethvenner@googlemail.com>
2016-11-14 17:20:31 +0000

    models and migrations

--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -2,10 +2,13 @@ require 'rails_helper'
 
 feature 'restaurants' do
   context 'no restaurants have been added' do
+    before do
+      Restaurant.create(name: 'KFC')
+    end
     scenario 'should display a prompt to add a restaurant' do
       visit 'restaurants'
-      expect(page).to have_content 'No restaurants yet'
-      expect(page).to have_link 'Add a restaurant'
+      expect(page).to have_content('KFC')
+      expect(page).not_to have_content('No restaurants yet')
     end
   end
 end
