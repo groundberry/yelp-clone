b330d9af6cae3d5639fd1c6522de64a0e1b3a3df
Blanca Mendizabal Perello <blanca.mendizabal.perello@gmail.com>
2016-11-17 15:46:31 +0000

    Add OmniAuth to allow login using a 3rd party login

--- Gemfile
+++ Gemfile
@@ -28,6 +28,8 @@ gem 'devise'
 
 gem 'dotenv-rails'
 
+gem 'omniauth-facebook'
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,6 +1,6 @@
 class ReviewsController < ApplicationController
 
-  before_action :authenticate_user!, :except => [:index, :show]
+  before_action :authenticate_user!
 
   def new
     @restaurant = Restaurant.find(params[:restaurant_id])
